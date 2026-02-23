cb3e5176af2aadf7dfd2ccf525dd972f4e8779b8
Shamim-Faqer) <219502059+Shamim-Faqer@users.noreply.github.com>
2026-02-23 19:19:06 +0600

    first-commit

---
+++ src/firebase/Firebase.init.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAdg_0rJUlLlfdvjWvCYyXejR9BJn6oUVM",
+  authDomain: "b12-a09-firesheild.firebaseapp.com",
+  projectId: "b12-a09-firesheild",
+  storageBucket: "b12-a09-firesheild.firebasestorage.app",
+  messagingSenderId: "1085180809110",
+  appId: "1:1085180809110:web:2ed0df911e644f8c42d7b7",
+  measurementId: "G-RXZV3HG902"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div><h2 className='text-5xl font-bold text-center py-5 bg-primary-content'>Ami Home</h2></div>
+  )
+}
+
+export default Home
