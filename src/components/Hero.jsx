e6bc4207f3503b169b8eb905d4a1103ef9bb3085
Shamim-Faqer) <219502059+Shamim-Faqer@users.noreply.github.com>
2026-02-26 12:34:33 +0600

    submit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,21 +1,33 @@
 import React from 'react'
 import heroImg from "../assets/hero.jpeg"
 import Banner from './Banner';
+import { motion } from "motion/react"
 
 function Hero() {
   return (
     <div>
           <div
-  className="min-h-screen  bg-cover bg-center flex items-end justify-center pb-6"
+  className="min-h-screen  bg-cover bg-center text-center flex items-end justify-center pb-6"
   style={{ backgroundImage: `url(${heroImg})` }}
 >
-  <div className="text-white p-6 text-center ">
+  <div >
     <Banner></Banner>
-    <h1 className="text-4xl md:text-6xl font-bold">The Ultimate Gaming Universe in One App</h1>
+{
+  <motion.h1
+  initial={{ opacity: 0, y: 50 }}
+  animate={{ opacity: 1, y: 0 }}
+  transition={{ duration: 1 }}
+  className='text-5xl font-bold'
+>
+ The Ultimate Gaming Universe in One App
+</motion.h1>
+}
     <p className="py-6">Discover, download, and play thousands of top-rated games instantly. 
   Everything you love about gaming — all in one powerful platform. Browse thousands of curated titles, fast downloads, secure installs, 
   and seamless updates.</p>
-    <button className="btn btn-primary text-white">Install</button>
+    <motion.button whileHover={{ scale: 1.1 }}
+  whileTap={{ scale: 0.95 }}
+  onHoverStart={() => console.log('hover started!')} className="btn btn-primary text-white">Install</motion.button>
   </div>
 </div>
     </div>
@@ -23,3 +35,5 @@ function Hero() {
 }
 
 export default Hero ;
+
+
